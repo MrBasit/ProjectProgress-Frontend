358df0034f0ad299270b073a550af25de0282d2a
Muhammad Umar <umar43umar@gmail.com>
2024-09-19 21:43:11 +0500

    setting headers

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -14,7 +14,15 @@ export class ProjectsService {
 
   constructor(private http: HttpClient){}
 
-  getProjectsByAccountName(accountId: number, pageNumber: number, pageSize: number,searchValue: string, sortValue : number, statusArr: any) {
+  private getHeaders() {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    const token = user.token;
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
+  getProjectsByAccountName(accountId: number, pageNumber: number, pageSize: number, searchValue: string, sortValue: number, statusArr: any) {
     const body = {
       sortDetails: {
         sortColumn: sortValue,
@@ -32,27 +40,31 @@ export class ProjectsService {
         id: status.id,
         name: status.name
       }))
-    }
-    return this.http.post<any>(`${this.url}/api/Projects/GetProjects`, body);
+    };
+
+    return this.http.post<any>(`${this.url}/api/Projects/GetProjects`, body, { headers: this.getHeaders() });
   }
-  
+
   getProjectById(projectId: number): Observable<any> {
     return this.http.get<any>(`${this.url}/api/Projects/GetProject`, {
-      params: { ProjectId: projectId.toString() }
+      params: { ProjectId: projectId.toString() },
+      headers: this.getHeaders()
     });
   }
 
   addProject(projectData: any): Observable<any> {
-    return this.http.post<any>(`${this.url}/api/Projects/AddProject`, projectData)
+    return this.http.post<any>(`${this.url}/api/Projects/AddProject`, projectData, { headers: this.getHeaders() });
   }
 
-  deleteProject(id: number){
-    return this.http.post<any>(`${this.url}/api/Projects/DeleteProject`, id)
+  deleteProject(id: number) {
+    return this.http.post<any>(`${this.url}/api/Projects/DeleteProject`, id, { headers: this.getHeaders() });
   }
+
   getStatuses(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.url}/api/Status/GetStatuses`);
+    return this.http.get<any[]>(`${this.url}/api/Status/GetStatuses`, { headers: this.getHeaders() });
   }
-  editProject(projectData: any){
-    return this.http.post(`${this.url}/api/Projects/EditProject`, projectData)
+
+  editProject(projectData: any) {
+    return this.http.post(`${this.url}/api/Projects/EditProject`, projectData, { headers: this.getHeaders() });
   }
 }
